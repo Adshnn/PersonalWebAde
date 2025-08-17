167f413f5679c2e79f597607e30ccf54079c864a
Amos-Situmorang14 <112601907+Adshnn@users.noreply.github.com>
2025-08-17 18:26:59 +0700

    OK

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content:[
+    "./*.html",
+    "./src/**/*.{html,js}",
+  ],
   theme: {
     container:{
       center: true,
